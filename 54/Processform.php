11bbc230330e4dff7fd3c4f3e6e1f048aec84743
Abraham G. Strauss <agstrauss89@gmail.com>
2021-06-28 19:32:50 -0400

    Add files via upload

---
+++ 54/Processform.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><META http-equiv="Content-Type" content="text/html; charset=utf-8"><style></style></head><body><u></u>
+
+
+
+    
+    
+    
+    
+    
+
+<header>
+    <p><a href="https://www.google.com/url?q=http://index.html&amp;source=gmail-html&amp;ust=1624492518598000&amp;usg=AFQjCNH-4fXEQS_qzVvhu05f425bBdHukQ" target="_blank" rel="noreferrer">Back</a></p>
+    <h1>Reservation Page</h1>
+</header>
+
+<div>
+    <h2>Please Fill Out The Form Below To Make A Reservation</h2>
+    <form action="http:/processForm.php" method="POST" target="_blank">
+    <p>What Date Would You Like To Reserve A Table?</p>
+    <label for="m_Date"> 
+        <input type="Date">
+    </label>
+    <p>What Time Would You Like To Reserve A Table?</p>
+    <label for="m_Time">
+        <input type="Time">
+    </label>
+    <p>How Many People Is This Reservation For? </p>
+    <label for="m_People">
+    <input type="Number"></label>
+    <p>Do You Perfer Indoor Or Outdoor Seating? </p>
+    <label for="m_Seating">Drink:</label>
+    <select name="Seating" id="m_Seat">
+      <option value="1">Outdoor</option>
+      <option>Indoor</option></select>
+     
+      <button type="reset">
+        Reset
+      </button>
+
+      <button type="submit">
+        Submit
+      </button>
+
+      </form>
+    
+         
+     
+
+</div>
+
+</body></html>
