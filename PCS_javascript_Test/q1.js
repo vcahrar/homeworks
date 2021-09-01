d2f29a798bcd7a8ceedd2f4661fc590acacc4815
Abraham G. Strauss <agstrauss89@gmail.com>
2021-09-01 19:54:45 -0400

    Add files via upload

---
+++ PCS_javascript_Test/q1.js
@@ -0,0 +1,22 @@
+
+    'use strict';
+    
+    function mapQuiz(inLine){
+        inLine =[2,3,9];
+    
+        //array to be acted upon
+
+        
+        for(let i = 0; i < inLine.length; i++){
+            
+            console.log('The square of ', inLine[i], 
+                ' is ',(inLine[i] * inLine[i]));
+
+            //The function has now been raised to the 2nd power
+    }
+    
+    
+    }
+    //[function call]
+    
+    mapQuiz();
